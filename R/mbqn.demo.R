c1aabb9cae058d226300b1e7676ab46743951660
Ariane Schad <ariane.schad@zbsa.de>
2018-02-01 13:51:50 +0100

    Add R package for MB-quantile normalization. Adapt Description file

--- R/mbqn.demo.R
+++ R/mbqn.demo.R
@@ -1,14 +1,14 @@
-#' Demo on mean-balanced quantile normalization
+#' Demo of mean/median-balanced quantile normalization
 #'
 #' @description This function demonstrates mean-balanced quantile normalization
-#' @param dat Default NULL. Optionally a user-supplied data matrix with rows - features, e.g. protein abundances; columns - samples. Data matrices can be computed with 
+#' @param dat Default NULL. Optionally a user-supplied data matrix with rows - features, e.g. protein abundances; columns - samples. Data matrices can be computed with
 #' mbqn.simu_dat()
 #' @return Various graphics illustrating saturation in case of quantile normalization of high expression for a single protein abundances across samples
 #' @keywords quantile normalization proteomics
-#' @references Schad, A. and Kreuz, C. (2017) Mean-balanced quantile normalization for processing label-free quantitative proteomics data with abundance-isolated proteins. Biostatistics xxx in prep. 
-#' @examples 
+#' @references Schad, A. and Kreuz, C. (2017) Mean-balanced quantile normalization for processing label-free quantitative proteomics data with abundance-isolated proteins. Biostatistics xxx in prep.
+#' @examples
 #' demo_mbqn
-#' demo_mbqn(dat) where dat is a data matrix where columns correspond to experimental samples or (technical/biological) replicates and rows correspond to features/proteins/peptides 
+#' demo_mbqn(dat) where dat is a data matrix where columns correspond to experimental samples or (technical/biological) replicates and rows correspond to features/proteins/peptides
 #' @details This function uses normalize.quantiles() from the package preprocessCore that can be installed from http://bioconductor.org/biocLite.R by
 #' source('http://bioconductor.org/biocLite.R')
 #' biocLite('preprocessCore')
@@ -20,14 +20,14 @@
 #biocLite('preprocessCore')
 
 mbqn.demo <- function(dat = NULL){
-  
+
   library(preprocessCore)
   if(is.null(dat)){
     # the function expects a matrix as input
     # create a matrix using the same example
     # each column corresponds to a sample
-    # each row corresponds to a protein 
-    
+    # each row corresponds to a protein
+
     dat <- matrix(c(5,2,3,NA,4,1,4,2,3,4,6,NA),
                   ncol=3)
     dat
@@ -38,15 +38,15 @@ mbqn.demo <- function(dat = NULL){
     #[4,]    4    2    8
   }
   dim(dat)
-  
+
   #quantile normalisation
   qn_dat <- normalize.quantiles(dat)
-  
+
   # now perform mean balanced qn
-  
+
   # sample mean for each row (protein)
   mdat <- apply(dat,1,mean,na.rm=TRUE)
-  
+
   #mean balanced quantile normalisation
   mbqn_dat <- normalize.quantiles(dat-mdat) + mdat
 }
