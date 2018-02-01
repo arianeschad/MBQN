aa54b47303402a2a010111b4d48a0c46070396af
Ariane Schad <ariane.schad@zbsa.de>
2018-02-01 15:20:42 +0100

    modified .R files and documentation, add to check_saturation a function for finding extreme abundancy proteins

--- R/mbqn.R
+++ R/mbqn.R
@@ -1,45 +1,39 @@
 #' Mean-balanced quantile normalization
 #'
-#' Function to compute a mean-balanced quantile normalization
-#' @param dat A data matrix. Rows - features, e.g. protein abundances; columns - samples
-#' @param FUN mean or median, or an array with dim = numbers of rows of dat. If left empty, quantile normalization is applied without balancing the data
-#' @return A matrix of mean- or median-balanced quantile normalized data
-#' @keywords quantile normalization proteomics
+#' Function to compute a mean-balanced quantile normalization.
+#' @param x A data matrix. Rows represent features, e.g. protein abundances; columns replicates, samples.
+#' @param FUN mean, median, or a user defined function, or an array with dim = numbers of rows of dat. If left empty, quantile normalization is applied without balancing the data.
+#' @return A matrix of mean- or median-balanced quantile normalized data.
+#' @keywords quantile normalization proteomics.
 #' @references Schad, A. and Kreuz, C. (2017) Mean-balanced quantile normalization for processing label-free quantitative proteomics data with abundance-isolated proteins. Biostatistics xxx in prep.
-#' @examples mbqn(dat, mean)
-#' mbqn(dat,median)
-#' mbqn(dat, x)
+#' @examples mbqn(x, mean)
+#' mbqn(x, median)
+#' mbqn(x, user_function)
+#' mbqn(x, user_array)
 #' @description This function uses normalize.quantiles() from the package preprocessCore that can be installed from http://bioconductor.org/biocLite.R
 #' @author A. Schad, Aug. 2017
 
 
-mbqn <- function(dat, FUN = NULL){
-  print("Compute mean balanced QN")
+mbqn <- function(x, FUN = NULL, na.rm = TRUE){
+
+  if (!is.matrix(x)) {
+    stop("Wrong data format! Input x must be a matrix!")
+  }
+
   if(!is.null(FUN)){
-    print("Compute mean balanced QN")
-    #library(preprocessCore)
-
     if(is.function(FUN)){
-    # sample mean for each row (protein)
-    mdat <- apply(dat,1,FUN,na.rm=TRUE)
-    }else if(is.function(FUN)){
-      mdat <- FUN
-      }
-    #mean balanced quantile normalisation
-    qn_dat <- preprocessCore::normalize.quantiles(dat-mdat) + mdat
-  }
-  else {
-  print("QN without mean balancing.")
-    #library(preprocessCore)
+
+      mx <- apply(x,1,FUN,na.rm=na.rm) # row mean
+
+    } else if(is.array(FUN)){
+      mx <- FUN
+    }
+    # balanced quantile normalisation
+    qn_x <- preprocessCore::normalize.quantiles(x-mx) + mx
+  } else {
+    print("Comput QN without mean balancing.")
     #quantile normalisation
-    qn_dat <- preprocessCore::normalize.quantiles(dat)
+    qn_x <- preprocessCore::normalize.quantiles(x)
   }
-  return(qn_dat)
+  return(qn_x)
 }
-
-
-#mbqn <- function(dat, FUN = mean_fun){
-#    mdat <- apply(dat,1,FUN,na.rm=TRUE)
-#   qn_dat <- normalize.quantiles(dat-mdat) + mdat
-#  return(qn_dat)
-#}
--- R/mbqn.check_saturation.R
+++ R/mbqn.check_saturation.R
@@ -1,6 +1,6 @@
-#' Test if normalization of data benefits from mean-balancing
+#' Test if quantile-normalization of data benefits from mean-balancing
 #'
-#' Compute a quantile normalization without and with mean-balancing and check standard deviation of normalized data entries located in the tails
+#' Rank data and test if lower and upper intensity tails are dominated by few feature types/proteins. Compute a quantile normalization without and with mean-balancing and check standard deviation of normalized data entries located in the tails
 #' @param dat A data matrix. Rows - features, e.g. protein abundances; columns - samples
 #' @param mean_fun mean or median, if left empty, quantile normalization is applied without balancing the data
 #' @param qlow lower quantile
@@ -13,11 +13,11 @@
 #' @author A. Schad, Aug. 2017
 
 
-
 mbqn.check_saturation <- function(dat, FUN = mean_fun, qlow, qup){
-  #library(preprocessCore)
+
   #quantile normalisation
   qn.dat <- preprocessCore::normalize.quantiles(dat)
+  mbqn(dat,FUN = mean, na.rm = TRUE)
   s.qn <- apply(qn.dat, 1, sd, na.rm=TRUE)
   # sample mean for each row (protein)
   m.dat <- apply(dat,1,FUN, na.rm=TRUE)
