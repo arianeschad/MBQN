4f67780a77d1ca515ffee5ad381d809e9c26df29
Ariane Schad <ariane.schad@zbsa.de>
2019-01-29 16:03:51 +0100

    update docu

--- R/example_NApattern.R
+++ R/example_NApattern.R
@@ -1,7 +1,7 @@
-#' Realistic missing value pattern of proteomic LFQ intensity samples
+#' Missing value pattern dataset
 #'
 #' An exemplary matrix of a missing value (MV) pattern extracted from
-#' measured LFQ intensities of the proteinGroups.txt dataset from PXD001584 \[1\].
+#' LFQ intensities of the proteinGroups.txt dataset from PXD001584 \[1\].
 #'
 #' @format A matrix of zero and ones with 1264 rows and 18 columns; 0 means MV, 1 means no MV.
 #' @source \url{https://www.ebi.ac.uk/pride/archive/projects/PXD001584}
