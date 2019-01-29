17c4a604346b0702a63cabab1218b6986c42e937
Ariane Schad <ariane.schad@zbsa.de>
2019-01-29 16:04:13 +0100

    update docu, set default flag

--- R/get_kminmax.R
+++ R/get_kminmax.R
@@ -1,26 +1,23 @@
-#' Get the k largest/smallest values and their indices for each
-#' column of a matrix
+#' Get indices and values of the k largest/smallest elements
 #'
-#' @description Extract the k largest/smallest values and their indices for each column of a matrix.
-#' @param X data matrix.
-#' @param k number of selected extreme values. Must be less then #rows of X.
-#' @param flag "min or "max"
-#' @details Function used by \code{mbqn.check_saturation}. Sort values of
-#' each column of a matrix or an array X and determine the
-#' k smallest (flag <- "min") or largest (flag <- "max") values in. NA's in the
+#' @description Extract the k largest or smallest values and their indices of each column of a matrix.
+#' @param X a data matrix.
+#' @param k integer specifying the number of selected extreme values. Must be less \code{<= nrows(X)}.
+#' @param flag "min" or "max"(default)
+#' @details Order the values of each column of X and determine the
+#' k smallest (\code{flag = "min"}) or largest (\code{flag = "max"}) values and their indices. NA's in the
 #' data are ignored.
 #' @return \code{list} with indices \code{ik} of extreme values \code{minmax}.
-# #' @keywords quantile normalization, proteomics
 #' @family mbqn
-#' @concept quantile, quantile normalization, rank invariance
+# #' @concept quantile, quantile normalization, rank invariance
 #' @references Schad, A. and Kreuz, C., MBQN: R package for mean balanced quantile normalization. In prep. 2019
 #' @examples
 #' X <- matrix(c(5,2,3,NA,4,1,4,2,3,4,6,NA,1,3,1),ncol=3) # Create a data matrix
 #' get_kminmax(X, k = 5, "max") # get indices of the 5 largest values in each column
-#' @author A. Schad, \email{ariane.schad@zbsa.de}
+#' @author Ariane Schad
 #  Aug. 2017
 #' @export get_kminmax
-get_kminmax <- function(X,k,flag){
+get_kminmax <- function(X,k,flag = "max"){
 
   if(flag == "min"){
     decreasing <- FALSE
