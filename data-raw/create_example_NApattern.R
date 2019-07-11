9fbd8af79bf03fa385533084ce95e5d57abbdb35
Admin <admin@rz-10-126-1-67.eduroam-rz.privat>
2019-07-11 08:47:24 +0200

    replaced deprecated mbqnLoadExample()

--- data-raw/create_example_NApattern.R
+++ data-raw/create_example_NApattern.R
@@ -1,8 +1,18 @@
 # Script file to create example file for NA pattern
 # A. Schad
 
-MBQN:::mbqnLoadExample(which.example = 1)
-mtx <- !is.na(mtx$mtx[,c(1:9,19:27)])
+pxd_id <- "PXD001584"
+
+# Load file
+out <- mbqnLoadFile(pxd_id, file.pattern = "proteinGroups.txt")
+
+# filter for potential contaminants and identified only by site features
+out <- out[!rowData(out)[["ixs"]],] 
+
+# extract data and feature annotation
+mtx <- assays(out)[["data"]]
+
+mtx <- !is.na(mtx[,c(1:9,19:27)])
 colnames(mtx) <- rownames(mtx) <- NULL
 six <- sort(apply(mtx,1,sum),decreasing = TRUE, index.return = TRUE)
 mtx <- ifelse(mtx[six$ix,], 1,0)
