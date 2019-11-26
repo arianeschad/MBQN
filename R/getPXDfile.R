df7f3dcff68953597221e061cb10683207c4783d
Eva Brombacher <eb162@pluto.uni-freiburg.de>
2019-11-26 21:15:02 +0100

    temporary fix due to changes in the rpx packages

--- R/getPXDfile.R
+++ R/getPXDfile.R
@@ -11,7 +11,8 @@
 #' @importFrom utils untar unzip
 #' @importFrom BiocFileCache bfcadd
 #' @return status (0=ok, 1=not MaxQuant data set, 2=no proteinGroup file)
-#' @details This function requires the R packages rpx \[2\] and BiocFileChace.
+#' @details This function requires the R packages rpx \[2\] and BiocFileChace. As a temporary
+#' fix old versions of the rpx package's pxurl and pxfiles function were included
 #' @references
 #' \[1\] Vizcaíno JA, Csordas A, del-Toro N, Dianes JA, Griss J, Lavidas I,
 #' Mayer G, Perez-Riverol Y, Reisinger F, Ternent T, Xu QW, Wang R, Hermjakob H.
@@ -27,27 +28,47 @@
 #' @export getPXDfile
 getPXDfile <- function(pxd_id, source.path = NULL,
                         file.pattern = "proteingroups"){
+    # from rpx version 1.21.1
+    pxurl <- function (object) {
+        stopifnot(inherits(object, "PXDataset"))
+        p <- "//cvParam[@accession = 'PRIDE:0000411']"
+        url <- xml2::xml_attr(xml2::xml_find_all(object@Data, p), "value")
+        names(url) <- NULL
+        print(url)
+        url
+    }
+
+    # from rpx version 1.21.1
+    pxfiles <- function (object) {
+        stopifnot(inherits(object, "PXDataset"))
+        ftpdir <- paste0(pxurl(object), "/")
+        ans <- strsplit(RCurl::getURL(ftpdir, dirlistonly = TRUE), "\n")[[1]]
+        if (Sys.info()["sysname"] == "Windows")
+            ans <- sub("\r$", "", ans)
+        ans
+    }
+
     px <- rpx::PXDataset(pxd_id)
-    
+
     if (is.null(source.path)) {source.path = file.path(getwd())}
     pdxFolder = file.path(source.path,pxd_id)
-    
+
     # Check if package preprocessCore is installed  to run this function
     if (!requireNamespace("rpx", quietly = TRUE)) {
         stop("Package \"pkg\" is required for this function to work.
             Please install it this package first!", call. = FALSE)}
-    
+
     # General Informations on PXD File:
     # rpx::pxtax(px)
     # rpx::pxurl(px) # url
     # rpx::pxref(px) # reference
-    
+
     status <- 0 # everything ok
-    
+
     # list files in repository
-    repoFiles <- rpx::pxfiles(px)
+    repoFiles <- pxfiles(px)
     #ind <- grep('MaxQuant',repoFiles)
-    ind <- grep('^(?!.*peptide).*maxquant|proteingroups.*$', 
+    ind <- grep('^(?!.*peptide).*maxquant|proteingroups.*$',
                 tolower(repoFiles), perl=TRUE)
     if (length(ind)==0){
         message("Only MaxQuant data formats are supported. Download stopped.")
@@ -56,34 +77,35 @@ getPXDfile <- function(pxd_id, source.path = NULL,
     }
     if (length(ind)>1)
         ind <- ind[1] # only the first match
-    
+
     destDirName <- "MBQN"
     destDir <- rappdirs::user_cache_dir(appname=destDirName)
     bfc <- BiocFileCache::BiocFileCache(destDir, ask=TRUE)
+    # rpx:::apply_fix_issue_5(FALSE)
     destFile <- BiocFileCache::bfcadd(bfc,
                                     pxd_id,
-                                    fpath=paste(rpx::pxurl(px),
+                                    fpath=paste(pxurl(px),
                                                 repoFiles[ind],sep="/"))
-    
+
     if (file.exists(destFile)){ # if zip file is available, start unzipping
         if(length(grep(".txt",repoFiles[ind])>0)){
             dir.create(file.path(pdxFolder), showWarnings = FALSE)
             # files = repoFiles[ind]
             file.copy(destFile, pdxFolder)
             unlink(destFile)
-            
+
         } else if (length(grep("tar.gz",repoFiles[ind])>0)){
             # which files are in archive
-            files = untar(destFile, list = TRUE) 
+            files = untar(destFile, list = TRUE)
             # only pattern match
-            files = files[grepl(file.pattern, files, ignore.case = TRUE)] 
-            
+            files = files[grepl(file.pattern, files, ignore.case = TRUE)]
+
             untar(destFile, files = files, exdir=)
-            
+
             # Change directory name to pxd id
             tryCatch(
                 {
-                    file.rename(file.path(source.path, 
+                    file.rename(file.path(source.path,
                                 sub("/[^/]+$", "", files)),
                                 file.path(pdxFolder))
                 },
@@ -91,7 +113,7 @@ getPXDfile <- function(pxd_id, source.path = NULL,
                     print(e)
                 }
             )
-            
+
             unlink(list.dirs(pdxFolder, recursive = FALSE),
                 recursive = TRUE) # delete extra folder
             unlink(destFile) # delete large .zip file
@@ -100,16 +122,16 @@ getPXDfile <- function(pxd_id, source.path = NULL,
         } else { #.gz
             files = unzip(destFile, list = TRUE) # which files are in archive
             # only pattern match
-            files = files$Name[grepl(file.pattern, files$Name, 
+            files = files$Name[grepl(file.pattern, files$Name,
                                     ignore.case = TRUE)]
-            
+
             if (length(files)>0){
                 unzip(destFile,files = files,exdir=pdxFolder)
-                
+
                 # Change directory name to pxd id
                 tryCatch(
                     {
-                        file.rename(file.path(source.path, 
+                        file.rename(file.path(source.path,
                                     sub("/[^/]+$", "", files)),
                                     file.path(pdxFolder))
                     },
@@ -117,7 +139,7 @@ getPXDfile <- function(pxd_id, source.path = NULL,
                         print(e)
                     }
                 )
-                
+
                 unlink(destFile) # delete large .zip file
                 # delete download folder
                 unlink(destDir,recursive = TRUE, force = TRUE)
--- R/mbqnSimuDistortion.R
+++ R/mbqnSimuDistortion.R
@@ -5,7 +5,7 @@
 #' @param x a matrix or data frame.
 #' @param s.mean scatter of relative change of mean.
 #' @param s.scale scatter of realtive change in scale, i.e. 0.01
-#' corresponds to 1\%.
+#' corresponds to 1 percent.
 #' @details Shift and scale the sample mean and standard deviation of a matrix.
 #' The perturbation of center and scale relative to mean and standard deviation
 #' of each sample are drawn from a Gaussian distribution \eqn{|N(0,\sigma^2)|}
@@ -26,21 +26,21 @@
 #' @author Ariane Schad
 #' @export mbqnSimuDistortion
 mbqnSimuDistortion <- function(x, s.mean = 0.05, s.scale = 0.01){
-    
+
     nc <- ncol(x)
     mx.offset <- abs(rnorm(nc))*s.mean+1
     mx.scale <- abs(rnorm(nc))*s.scale+1
-    
+
     # distort matrix
     mx <- apply(x,2,mean,na.rm =TRUE)
     # x.scaled <- sapply(seq_len(nc), function(i) (x[,i]-mx[i])*mx.scale[i])
     x.scaled <- vapply(seq_len(nc), function(i) (x[,i]-mx[i])*mx.scale[i],
                     FUN.VALUE = numeric(nrow(x)))
-    
+
     # x.mod <- sapply(seq_len(nc), function(i) (x.scaled[,i]+(mx*mx.offset)[i]))
     x.mod <- vapply(seq_len(nc), function(i) (x.scaled[,i]+(mx*mx.offset)[i]),
                     FUN.VALUE = numeric(nrow(x.scaled)))
-    
+
     return(distort = list(x.mod = x.mod, mx.offset = mx.offset,
                         mx.scale = mx.scale))
 }
